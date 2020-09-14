34b21e5c0aca807fb72b663137f327ef1f18c77b
liujinqiang <1414421923@qq.com>
2020-09-14 23:51:05 +0800

    创建一个项目

---
+++ git-learn-demo1/src/com/liu/demo.java
@@ -0,0 +1,11 @@
+package com.liu;
+
+/**
+ * @author liujinqiang
+ * @create 2020-09-14 23:49
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("cao");
+    }
+}
